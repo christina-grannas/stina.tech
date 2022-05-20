189e6635751a5ac840e69f09faf10da9af96046b
Christina Grannas <christina.grannas@gmail.com>
2022-05-20 22:32:46 +0200

    Updates

--- src/routes/blog/azure-diagram-template-in-figjam.md
+++
@@ -1,3 +0,0 @@
-## Azure diagram template in FigJam
-
-FigJam has a template for designing Microsoft Azure Diagrams! There are also templates for Google Cloud and AWS.
--- src/routes/blog/index.svelte
+++
@@ -1,9 +0,0 @@
-<h1>Welcome to my blawg</h1>
-<ul>
-    <li>Make blog in SvelteKit + md files</li>
-    <li>Put in github repo</li>
-    <li>Build it somehow</li>
-    <li>Host on github pages</li>
-    <li>Images?</li>
-    <li>Art part?</li>
-</ul>
