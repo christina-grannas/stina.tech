2103012ccc7da4170ef3b193188712abc368d484
= <christina.grannas@isotop.se>
2022-05-20 21:41:06 +0200

    Adding mdsvex

---
+++ src/routes/blog/azure-diagram-template-in-figjam.md
@@ -0,0 +1,3 @@
+## Azure diagram template in FigJam
+
+FigJam has a template for designing Microsoft Azure Diagrams! There are also templates for Google Cloud and AWS.
---
+++ src/routes/blog/index.svelte
@@ -0,0 +1,9 @@
+<h1>Welcome to my blawg</h1>
+<ul>
+    <li>Make blog in SvelteKit + md files</li>
+    <li>Put in github repo</li>
+    <li>Build it somehow</li>
+    <li>Host on github pages</li>
+    <li>Images?</li>
+    <li>Art part?</li>
+</ul>
